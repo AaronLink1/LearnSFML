d34c1f1235f7a961d03a28dc69a94bbf922b5b17
Aaron <aaron@thelinkfamily.net>
2025-07-21 20:40:35 -0500

    Verified SFML worked and moved dll's to appropriate files

--- LearnSFML/src/main.cpp
+++ LearnSFML/src/main.cpp
@@ -1,8 +1,23 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	printf("Initial Setup Complete.");
+	sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML Works!");
+	sf::CircleShape circle(100.f);
+	circle.setFillColor(sf::Color::Blue);
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
 
 	return 0;
 }
