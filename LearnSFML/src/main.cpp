ba1050d3c574f23b61dc51c997f15fc1dc169820
Aaron <aaron@thelinkfamily.net>
2025-09-07 21:33:57 -0500

    Added font and text elements

--- LearnSFML/src/main.cpp
+++ LearnSFML/src/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <numbers>
 #include <iostream>
+#include <sstream>
 
 constexpr double RAD_TO_DEG (180.0 / std::numbers::pi);
 
@@ -10,14 +11,45 @@ int main()
 	sf::RenderWindow window(sf::VideoMode({ 1980, 1080 }), "SFML Learning Vectors");
 	window.setVerticalSyncEnabled(true);
 
-	//sf::Clock gameClock;
-
 	sf::RectangleShape rect({0.0f, 5.0f});
 	rect.setOrigin({ 0.0f, rect.getSize().y / 2.0f });
 	rect.setPosition({ window.getSize().x / 2.0f, window.getSize().y / 2.0f });
 	rect.setFillColor(sf::Color::Red);
 	rect.setOutlineThickness(1.0f);
 	rect.setOutlineColor(sf::Color::Black);
+
+	sf::Font font;
+	if (!font.openFromFile("../resources/fonts/prototype/prototype.ttf"))
+	{
+		std::cerr << "Error: Could not load file {../resources/fonts/prototype/prototype.ttf}" << std::endl;
+	}
+
+	std::stringstream sStream;
+	sStream << "Vector 1: (" << rect.getSize().x << ", " << rect.getSize().y << ")";
+
+	sf::Text vectorOne(font);
+	vectorOne.setString(sStream.str());
+	vectorOne.setOrigin(vectorOne.getGlobalBounds().size / 2.0f);
+	vectorOne.setPosition({ window.getSize().x * 0.25f, 50.0f});
+	vectorOne.setFillColor(sf::Color::Red);
+	vectorOne.setOutlineThickness(1.0f);
+	vectorOne.setOutlineColor(sf::Color::Black);
+
+	sf::Text vectorTwo(font);
+	vectorTwo.setString(sStream.str());
+	vectorTwo.setOrigin(vectorTwo.getGlobalBounds().size / 2.0f);
+	vectorTwo.setPosition({ window.getSize().x * 0.5f, 50.0f });
+	vectorTwo.setFillColor(sf::Color::Red);
+	vectorTwo.setOutlineThickness(1.0f);
+	vectorTwo.setOutlineColor(sf::Color::Black);
+
+	sf::Text vectorThree(font);
+	vectorThree.setString(sStream.str());
+	vectorThree.setOrigin(vectorThree.getGlobalBounds().size / 2.0f);
+	vectorThree.setPosition({ window.getSize().x * 0.75f, 50.0f });
+	vectorThree.setFillColor(sf::Color::Red);
+	vectorThree.setOutlineThickness(1.0f);
+	vectorThree.setOutlineColor(sf::Color::Black);
 
 	while (window.isOpen())
 	{
@@ -46,10 +78,20 @@ int main()
 			sf::Vector2f direction = mousePosition - rect.getPosition();
 			rect.setSize({ std::hypotf(direction.x, direction.y), rect.getSize().y });
 			rect.setRotation(sf::degrees(std::atan2(direction.y, direction.x) * RAD_TO_DEG));
+
+			sStream.str("");
+			sStream.clear();
+			sStream << "Vector 1: (" << direction.x << ", " << direction.y << ")";
+			vectorOne.setString(sStream.str());
+			vectorTwo.setString(sStream.str());
+			vectorThree.setString(sStream.str());
 		}
 
 		window.clear(sf::Color::Cyan);
 		window.draw(rect);
+		window.draw(vectorOne);
+		window.draw(vectorTwo);
+		window.draw(vectorThree);
 		window.display();
 	}
 
